283ffd32b1d9527ecaf01eaf6eec99db47b009ba
kamel <mega001@mail.com>
2023-08-18 06:49:18 -0700

    fix betty style shell.c

--- shell.c
+++ shell.c
@@ -1,4 +1,43 @@
 #include "shell.h"
+/**
+ * ispath - execute when pathnam true
+ * @arr: array of pointer of pointers
+ * @path: pointer of chars
+*/
+
+void ispath_nam(char **arr, char *path)
+{
+	free(arr[0]);
+	arr[0] = path;
+	exec(arr);
+}
+
+/**
+ * isfunc - execute when func true
+ * @arr: array of pointer of pointers
+ * @buf: pointer of chars
+*/
+
+void isfunc(char *buf, char **arr)
+{
+	free(buf);
+	f(arr);
+}
+
+/**
+ * man_dir - manipulating directory paths
+ * @v: value
+ * @head: pointer to h
+ * @path: pth nam
+ * @arr: arr_v
+ */
+
+void man_dir(char *v, path_dir *head, char *path, char **arr)
+{
+	v = _getenv("PATH");
+	head = dir_path(v);
+	path = get_path(arr[0], head);
+}
 
 /**
  * main - shell logic
@@ -8,39 +47,33 @@
 int main(void)
 {
 	void (*func)(char **);
-	char *buffer = NULL, *val, *pth_nam;
-	char **arr_v;
+	char **arr_v, *buffer = NULL, *val, *pth_nam;
 	size_t size = 0;
 	ssize_t cmdlen = 0;
 	path_dir *h = '\0';
 
+	signal(SIGINT, ctrl_c);
 	while (cmdlen != EOF)
 	{
 		_isterminal();
 		cmdlen = getline(&buffer, &size, stdin);
 		_isEOF(cmdlen, buffer);
-		arr_v = splitstring(buffer, " \n");
-
+		arr_v = splitstr(buffer, " \n");
 		if (!arr_v || !arr_v[0])
 		{
 			exec(arr_v);
 		}
 		else
 		{
-			val = _getenv("PATH");
-			h = dir_path(val);
-			pth_nam = get_path(arr_v[0], h);
+			man_dir(val, h, pth_nam, arr_v);
 			func = verify_build(arr_v);
 			if (func)
 			{
-				free(buffer);
-				f(arr_v);
+				isfunc(buffer, arr_v);
 			}
 			else if (pth_nam)
 			{
-				free(arr_v[0]);
-				arr_v[0] = pth_nam;
-				exec(arr_v);
+				ispath_nam(arr_v, pth_nam);
 			}
 			else if (!pth_nam)
 			{
